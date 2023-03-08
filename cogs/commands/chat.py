15170b5be30449bc4f80c595f1ab186cea5dee20
mikeli1117 <mikeli1117@gmail.com>
2023-03-08 15:23:53 +0800

    adde cog file system

--- cogs/commands/chat.py
+++ cogs/commands/chat.py
@@ -1,34 +1,61 @@
 import discord
 from discord.ext import commands
-from events.onMessage import erase
+from cogs.commands.ask import erase
+from cogs.commands.ask import update_prompt
+import json
 
-class Chat(commands.Cog): # create a class for our cog that inherits from commands.Cog
-    # this class is used to create a cog, which is a module that can be added to the bot
+class Chat(commands.Cog):
 
-    def __init__(self, bot): # this is a special method that is called when the cog is loaded
+    def __init__(self, bot):
         self.bot = bot
 
-    @discord.slash_command(description="Sends the bot's latency.") # this decorator makes a slash command
-    async def erase(self, ctx): # a slash command will be created with the name "ping"
+    @discord.slash_command(description="Tutorial for interacting with chatbot")
+    async def help(self ,ctx):
+        await ctx.defer()
+        await ctx.respond("/ai One-time conversation without memory \n/ask Continuous conversation with memory \n/erase to wipe the restart conversation \n/model to select chat model ")
+
+
+    @discord.slash_command(name="prompt", description="Predefine bot's behavior")
+    async def prompt(self,
+        ctx,
+        prompt: discord.Option(discord.SlashCommandOptionType.string)
+    ):
+        await update_prompt(prompt)
+        await ctx.respond(f"New prompt: `{prompt}`.")
+
+
+    @discord.slash_command(description="Erase Conversation Memory")
+    async def erase(self, ctx):
+        await ctx.defer()
         await erase()
-        await ctx.respond(f"ChatBot memory erased!")
+        await ctx.respond(f"{ctx.author.mention}, ChatBot memory erased!")
 
 
     async def get_ai_model(ctx: discord.AutocompleteContext):
-        ai_model = ctx.options['ai_model']
-        if ai_model == 'Marine':
-            return ['Whale', 'Shark', 'Fish', 'Octopus', 'Turtle']
-        else: # is land animal
-            return ['Snake', 'Wolf', 'Lizard', 'Lion', 'Bird']
+        return ["gpt-3.5-turbo", "gpt-3.5-turbo-0301", "text-davinci-003", "text-davinci-002"]
 
-    @discord.slash_command(name="animal")
-    async def animal_command(
+    @discord.slash_command(name="model", description="Select AI Model")
+    async def model_command( self,
     ctx: discord.ApplicationContext,
-    ai_model: discord.Option(str, choices=['Marine', 'Land']),
-    animal: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(get_ai_model))
+    ai_model: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(get_ai_model), description="Select AI Model", required = False, default = '')
     ):
-        await ctx.respond(f'You picked an model type of `{ai_model}`!')
+        await ctx.defer()
+        with open('config.json', "r+") as f:
+            data = json.load(f)
+
+            
+            if (ai_model!=''):
+                data['default_model'] = ai_model
+                f.seek(0)
+                json.dump(data, f, indent=4)
+                f.truncate()
+                await ctx.respond(f'{ctx.author.mention}, Chatbot model is switched to `{ai_model}`!')
+                return
+            curr_model=data['default_model']
+            await ctx.respond(f'{ctx.author.mention}, The current chatbot model is `{curr_model}`!')
 
 
-def setup(bot): # this is called by Pycord to setup the cog
-    bot.add_cog(Chat(bot)) # add the cog to the bot
+    
+
+def setup(bot):
+    bot.add_cog(Chat(bot))
--- main.py
+++ main.py
@@ -1,120 +1,25 @@
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
-
-from events.onMessage import onMessage
-from events.onMessage import erase
-from events.onReady import onReady
-import json
-
-from discord.ext import commands
-
-# uncomfirmed
-from enum import Enum
-
 
 
 # load .env
 load_dotenv() 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-OPENAI_TOKEN = os.getenv('OPENAI_TOKEN')
 
 # Intents
 intents = discord.Intents.default()
 intents.members = True
 intents.presences = True
 intents.message_content = True
-# bot = discord.Bot(intents=intents) #main worked, greeting dont, cog error
-bot = commands.Bot(command_prefix="!", intents=intents) #@bot.command() doesnt, @bot.slashcommand() worked, 
+bot = commands.Bot(command_prefix='/', help_command=None, intents=intents) 
 
+# Load events, commands
+for foldername in os.listdir('./cogs'): #for every folder in cogs
+    for filename in os.listdir(f'./cogs/{foldername}'):# for every file in a folder in cogs
+        if filename.endswith('.py') and not filename in ['util.py', 'error.py']: #if the file is a python file and if the file is a cog
+            bot.load_extension(f'cogs.{foldername}.{filename[:-3]}')#load the extension
 
-
-
-#typing when loading?
-
-# tree = app_commands.CommandTree(client)
-
-
-# @tree.command(name = "help", description = "Tutorial of the chatbot", guild=discord.Object(id=729915471113224222)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
-# async def help_command(interaction):
-#     await interaction.response.send_message("/ai One-time conversation without memory \n/ask Continuous conversation with memory \n/erase to wipe the restart conversation \n/model to select chat model ")
-
-# @tree.command(name = "erase", description = "Erase conversation memory", guild=discord.Object(id=729915471113224222)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
-# async def erase_command(interaction):
-#     await erase()
-#     await interaction.response.send_message(f'<@{interaction.user.id}>, chatbot memory erased! ')
-
-
-# ai_models = Enum(value='ai_models', names=["gpt-3.5-turbo", "gpt-3.5-turbo-0301", "text-davinci-003", "text-davinci-002"])
-# @tree.command(description='Select the model for the chatbot', guild=discord.Object(729915471113224222))
-# @discord.app_commands.describe(model='Select the model for the chatbot')
-# async def model(interaction: discord.Interaction, model: Enum(value='ai_models', names=["gpt-3.5-turbo", "gpt-3.5-turbo-0301", "text-davinci-003", "text-davinci-002"])):
-#     user = interaction.user.id
-#     with open('config.json', "r+") as f:
-#         data = json.load(f)
-
-#         if model.name == 'gpt-3.5-turbo':
-#             data['default_model'] = 'gpt-3.5-turbo'
-#             await interaction.response.send_message(f'<@{user}>, Model is switched to gpt-3.5-turbo!')
-#         elif model.name == 'gpt-3.5-turbo-0301':
-#             data['default_model'] = 'gpt-3.5-turbo-0301'
-#             await interaction.response.send_message(f'<@{user}>, Model is switched to gpt-3.5-turbo-0301!')
-#         elif model.name == 'text-davinci-003':
-#             data['default_model'] = 'text-davinci-003'
-#             await interaction.response.send_message(f'<@{user}>, Model is switched to text-davinci-003!')
-#         elif model.name == 'text-davinci-002':
-#             data['default_model'] = 'text-davinci-002'
-#             await interaction.response.send_message(f'<@{user}>, Model is switched to text-davinci-002!')
-        
-#         f.seek(0)
-#         json.dump(data, f, indent=4)
-#         f.truncate()
-    
-# @bot.command(description="Sends the bot's latency.") # this decorator makes a slash command
-# async def ping(ctx): # a slash command will be created with the name "ping"
-#     await ctx.respond(f"Pong! Latency is {bot.latency}")
-
-
-# @bot.command()
-# # pycord will figure out the types for you
-# async def add(ctx, first: discord.Option(int), second: discord.Option(int)):
-#   # you can use them as they were actual integers
-#   sum = first + second
-#   await ctx.respond(f"The sum of {first} and {second} is {sum}.")
-
-
-@bot.event
-async def on_ready():
-    await onReady(bot)
-
-
-# async def get_animal_types(ctx: discord.AutocompleteContext):
-#   """
-#   Here we will check if 'ctx.options['animal_type']' is a marine or land animal and return respective option choices
-#   """
-#   animal_type = ctx.options['animal_type']
-#   if animal_type == 'Marine':
-#     return ['Whale', 'Shark', 'Fish', 'Octopus', 'Turtle']
-#   else: # is land animal
-#     return ['Snake', 'Wolf', 'Lizard', 'Lion', 'Bird']
-
-# @bot.slash_command(name="animal")
-# async def animal_command(
-#   ctx: discord.ApplicationContext,
-#   animal_type: discord.Option(str, choices=['Marine', 'Land']),
-#   animal: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(get_animal_types))
-# ):
-#   await ctx.respond(f'You picked an animal type of `{animal_type}` that led you to pick `{animal}`!')
-    
-
-@bot.event
-async def on_message(message):
-    await onMessage(bot, message, OPENAI_TOKEN)
-
-for filename in os.listdir('./cogs'): #for every folder in cogs
-      if filename.endswith('.py') and not filename in ['util.py', 'error.py']: #if the file is a python file and if the file is a cog
-          bot.load_extension(f'cogs.{filename[:-3]}')#load the extension
-
-
-bot.load_extension('cogs.commands.chat')
+# Launch bot
 bot.run(DISCORD_TOKEN)
